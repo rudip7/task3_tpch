228b3ae395e5b37af6b813b452f5a94bdc40ebf3
rudip7 <rudipoepsel7rpl@hotmail.com>
2019-07-01 15:06:37 +0200

    Fuck this shit

--- src/task4.cpp
+++ src/task4.cpp
@@ -20,65 +20,6 @@ namespace task4{
         uint64_t count_order;
     };
 
-  void query1_filter(const uint32_t* shipdate,
-                    std::vector<int> &indexes,
-                     unsigned int beginIndex, unsigned int endIndex){
-      for (unsigned int j = beginIndex; j < endIndex; ++j)
-      {
-          if(shipdate[j] <= 19980902) {
-              indexes.push_back(j);
-          }
-      }
-  }
-
-  void query1_group(const char* returnflag,
-                    const char* linestatus,
-                    std::vector<int> &indexes,
-                    std::unordered_map<short, std::vector<int>> &hm){
-      for (int &i : indexes){
-          short key = (((short) returnflag[i]) << 8) | ((short) linestatus[i]);
-          std::unordered_map<short, std::vector<int>>::const_iterator got = hm.find(key);
-          if ( got == hm.end() ) {
-              std::vector<int> groupIndexes{i};
-              hm[key] = groupIndexes;
-          }else {
-              hm.at(key).push_back(i);
-          }
-
-      }
-  }
-
-  void query1_compute(const uint16_t* quantity,
-                      const float * discount,
-                      const float * extendedprice,
-                      const float* tax,
-                      std::unordered_map<short, std::vector<int>> &hm,
-                      std::unordered_map<short, part_record> &part_result){
-
-
-        int num = 0;
-      for (auto& it: hm)
-      {
-          short key = it.first;
-          part_record* record = static_cast<part_record *>(malloc(sizeof(part_record)));
-          record->linestatus = key;
-
-          key = key>>8;
-          record->returnflag = key;
-          record->count_order = it.second.size();
-          for (int &i : it.second){
-              record->sum_qty = record->sum_qty + quantity[i];
-              record->sum_base_price = record->sum_base_price + extendedprice[i];
-              double disc_price = extendedprice[i] *(1-discount[i]);
-              record->sum_disc_price = record->sum_disc_price + disc_price;
-              record->sum_charge = record->sum_charge + disc_price * (1+tax[i]);
-              record->sum_disc = record->sum_disc + discount[i];
-          }
-          part_result[it.first] = *record;
-      }
-
-  }
-
   /** \brief implement these three functions */
   void query1_function(const uint16_t* quantity,
                         const char* returnflag,
@@ -87,7 +28,8 @@ namespace task4{
                        const float * discount,
                        const float * extendedprice,
                        const float* tax,
-                       std::unordered_map<short, part_record> &hm,
+                       std::unordered_map<short, short> &hm,
+                       std::vector<part_record> &part_result,
                        unsigned int beginIndex, unsigned int endIndex,
                        unsigned int stride){
       for (unsigned int j = beginIndex; j < endIndex; j+=stride)
@@ -95,8 +37,9 @@ namespace task4{
           if(shipdate[j] <= 19980902) {
               short key = (((short) returnflag[j]) << 8) | ((short) linestatus[j]);
 
-              std::unordered_map<short, part_record>::const_iterator got = hm.find(key);
+              std::unordered_map<short, short>::const_iterator got = hm.find(key);
               if ( got == hm.end() ) {
+                  hm[key] = part_result.size();
                  // std::cout << "Key: " << key << " corresponds to " << returnflag[j] << " and " << linestatus[j] << std::endl;
                   part_record* record = static_cast<part_record *>(malloc(sizeof(part_record)));
                   record->sum_qty = quantity[j];
@@ -107,17 +50,19 @@ namespace task4{
                   record->count_order = 1.0;
                   record->returnflag = returnflag[j];
                   record->linestatus = linestatus[j];
+                  part_result.push_back(*record);
 
-                  hm[key] = *record;
+
               }else {
 //                  part_record* record = static_cast<part_record *>(malloc(sizeof(part_record)));
-                  hm.at(key).sum_qty = got->second.sum_qty + quantity[j];
-                  hm.at(key).sum_base_price = got->second.sum_base_price + extendedprice[j];
+                    short index = got->second;
+                  part_result[index].sum_qty += quantity[j];
+                  part_result[index].sum_base_price += extendedprice[j];
                   double disc_price = extendedprice[j] *(1-discount[j]);
-                  hm.at(key).sum_disc_price = got->second.sum_disc_price + disc_price;
-                  hm.at(key).sum_charge = got->second.sum_charge + disc_price * (1+tax[j]);
-                  hm.at(key).sum_disc = got->second.sum_disc + discount[j];
-                  hm.at(key).count_order = got->second.count_order + 1.0;
+                  part_result[index].sum_disc_price += disc_price;
+                  part_result[index].sum_charge += disc_price * (1+tax[j]);
+                  part_result[index].sum_disc += discount[j];
+                  part_result[index].count_order += 1.0;
 
               }
           }
@@ -148,9 +93,10 @@ namespace task4{
 
       int numThreads = std::thread::hardware_concurrency();
 
-      std::unordered_map<short, part_record> hm[numThreads];
+      std::unordered_map<short, short> hm[numThreads];
 
       std::thread threads[numThreads];
+      std::vector<part_record> part_result[numThreads];
 
 
 
@@ -183,6 +129,7 @@ namespace task4{
                                    std::ref(extendedprice),
 
                                    std::ref(tax), std::ref(hm[k]),
+                                   std::ref(part_result[k]),
 
                                    k, num_records, numThreads);
 
@@ -196,42 +143,36 @@ namespace task4{
 
       }
 
-      std::map<short, part_record> final_ht;
+      std::map<short, short> final_ht;
+      std::vector<part_record> final_result;
 
       for (int j = 0; j < numThreads; ++j) {
 
           for (auto& x: hm[j]) {
 
               short key = x.first;
+              short index = x.second;
+              part_record &tuple = part_result[j].at(index);
 
-              std::map<short, part_record>::const_iterator got = final_ht.find(key);
+              std::map<short, short>::const_iterator got = final_ht.find(key);
 
               if ( got == final_ht.end() ) {
 
-                  final_ht[x.first] = x.second;
+                  final_ht[x.first] = final_result.size();
+                  final_result.push_back(tuple);
 
               }else {
+                  final_result[got->second].sum_qty += tuple.sum_qty;
 
-//                  part_record* record = static_cast<par
+                  final_result[got->second].sum_base_price += tuple.sum_base_price;
 
-//                  t_record *>(malloc(sizeof(part_record)));
+                  final_result[got->second].sum_disc_price += tuple.sum_disc_price;
 
-                  final_ht.at(key).sum_qty = got->second.sum_qty + x.second.sum_qty;
+                  final_result[got->second].sum_charge += tuple.sum_charge;
 
-                  final_ht.at(key).sum_base_price = got->second.sum_base_price + x.second.sum_base_price;
+                  final_result[got->second].sum_disc += tuple.sum_disc;
 
-
-
-                  final_ht.at(key).sum_disc_price = got->second.sum_disc_price + x.second.sum_disc_price;
-
-                  final_ht.at(key).sum_charge = got->second.sum_charge + x.second.sum_charge;
-
-                  final_ht.at(key).sum_disc = got->second.sum_disc + x.second.sum_disc;
-
-                  final_ht.at(key).count_order = got->second.count_order + x.second.count_order;
-
-
-
+                  final_result[got->second].count_order += tuple.count_order;
               }
 
           }
@@ -243,26 +184,26 @@ namespace task4{
       for (auto& x: final_ht) {
 
           tpch_query_1_result::record* record = static_cast<tpch_query_1_result::record *>(malloc(sizeof(tpch_query_1_result::record)));
+          part_record res = final_result[x.second];
+          record->returnflag = res.returnflag;
 
-          record->returnflag = x.second.returnflag;
+          record->linestatus = res.linestatus;
 
-          record->linestatus = x.second.linestatus;
+          record->sum_qty = res.sum_qty;
 
-          record->sum_qty = x.second.sum_qty;
+          record->sum_base_price = res.sum_base_price;
 
-          record->sum_base_price = x.second.sum_base_price;
+          record->sum_disc_price = res.sum_disc_price;
 
-          record->sum_disc_price = x.second.sum_disc_price;
+          record->sum_charge = res.sum_charge;
 
-          record->sum_charge = x.second.sum_charge;
+          record->avg_qty = res.sum_qty / res.count_order;
 
-          record->avg_qty = x.second.sum_qty / x.second.count_order;
+          record->avg_price = res.sum_base_price / res.count_order;
 
-          record->avg_price = x.second.sum_base_price / x.second.count_order;
+          record->avg_disc = res.sum_disc / res.count_order;
 
-          record->avg_disc = x.second.sum_disc / x.second.count_order;
-
-          record->count_order = x.second.count_order;
+          record->count_order = res.count_order;
 
 
 
@@ -273,105 +214,6 @@ namespace task4{
           result.records.push_back(*record);
 
       }
-      /*int numThreads = std::thread::hardware_concurrency();
-      std::thread threads[numThreads];
-    size_t num_records = l.size();
-      const uint32_t* shipdate = l.shipdate.data();
-      std::vector<int> indexes[numThreads];
-
-      for (int k = 0; k < numThreads; ++k) {
-          unsigned int endIndex;
-          if (k < numThreads-1){
-              endIndex = (k+1)*(num_records+numThreads-1)/numThreads;
-          } else{
-              endIndex = num_records;
-          }
-
-          threads[k] = std::thread(query1_filter,
-                                   std::ref(shipdate),
-                                   std::ref(indexes[k]),
-                                   k*(num_records+numThreads-1)/numThreads, endIndex);
-      }
-      for (int m = 0; m < numThreads; ++m) {
-          threads[m].join();
-      }
-      //PHASE 2 BUILD GROUPS
-      const char* returnflag = l.returnflag.data();
-      const char* linestatus = l.linestatus.data();
-      std::unordered_map<short, std::vector<int>> hm[numThreads];
-
-      for (int k = 0; k < numThreads; ++k) {
-          threads[k] = std::thread(query1_group,
-                                   std::ref(returnflag),
-                                   std::ref(linestatus),
-                                   std::ref(indexes[k]),
-                                   std::ref(hm[k]));
-      }
-      for (int m = 0; m < numThreads; ++m) {
-          threads[m].join();
-      }
-
-    const uint16_t* quantity = l.quantity.data();
-      const float * discount = l.discount.data();
-      const float * extendedprice = l.extendedprice.data();
-      const float* tax = l.tax.data();
-
-      std::unordered_map<short, part_record> part_result[numThreads];
-
-      for (int k = 0; k < numThreads; ++k) {
-          threads[k] = std::thread(query1_compute, std::ref(quantity),
-                                   std::ref(discount),
-                                   std::ref(extendedprice),
-                                   std::ref(tax),
-                                   std::ref(hm[k]),
-                                   std::ref(part_result[k]));
-      }
-
-      for (int m = 0; m < numThreads; ++m) {
-          threads[m].join();
-      }
-
-      std::map<short, part_record> final_ht;
-      for (int j = 0; j < numThreads; ++j) {
-          for (auto& x: part_result[j]) {
-              short key = x.first;
-              std::map<short, part_record>::const_iterator got = final_ht.find(key);
-              if ( got == final_ht.end() ) {
-                  final_ht[x.first] = x.second;
-              }else {
-                  final_ht.at(key).sum_qty = got->second.sum_qty + x.second.sum_qty;
-                  final_ht.at(key).sum_base_price = got->second.sum_base_price + x.second.sum_base_price;
-
-                  final_ht.at(key).sum_disc_price = got->second.sum_disc_price + x.second.sum_disc_price;
-                  final_ht.at(key).sum_charge = got->second.sum_charge + x.second.sum_charge;
-                  final_ht.at(key).sum_disc = got->second.sum_disc + x.second.sum_disc;
-                  final_ht.at(key).count_order = got->second.count_order + x.second.count_order;
-
-              }
-          }
-      }
-
-      for (auto& x: final_ht) {
-          tpch_query_1_result::record* record = static_cast<tpch_query_1_result::record *>(malloc(sizeof(tpch_query_1_result::record)));
-          record->returnflag = x.second.returnflag;
-          record->linestatus = x.second.linestatus;
-          record->sum_qty = x.second.sum_qty;
-          record->sum_base_price = x.second.sum_base_price;
-          record->sum_disc_price = x.second.sum_disc_price;
-          record->sum_charge = x.second.sum_charge;
-          record->avg_qty = x.second.sum_qty / x.second.count_order;
-          record->avg_price = x.second.sum_base_price / x.second.count_order;
-          record->avg_disc = x.second.sum_disc / x.second.count_order;
-          record->count_order = x.second.count_order;
-
-         // std::cout << record->toString() << std::endl;
-
-          result.records.push_back(*record);
-      }*/
-
-
-
-
   }
 
   void tpch_query5_impl(const columnstore::lineitem& l,
